7ffe1a8130605ffd911548ad5e8855712788dfc3
Konrad Przybysz <k.przybysz@atcomp.pl>
2022-07-08 10:34:01 +0200

    poprwaiona onfocus i input walidacja

--- DAY 1/js/form.js
+++ DAY 1/js/form.js
@@ -1,3 +1,44 @@
+const defaultValidation = (value) => {
+    return /.+/.test(value)
+}
+
+const peselValidation = (value) => {
+    let weights = '1379137913';
+    let checkSum = 0;
+    if(/^\d{11}$/.test(value)){
+        let i=0;
+        let tempSum;
+        for(i; i<10; i++){
+            tempSum = value[i] * weights[i];
+            tempSum = tempSum % 10;
+            checkSum += tempSum;
+        }
+        checkSum = checkSum % 10;
+        finalCheck = 10 - checkSum;
+        if(finalCheck == value[10]){
+            return true;
+        }
+    }
+}
+
+const regForEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+let inputs = {
+    name: {label: 'Imię', validationF: defaultValidation},
+    surname: {label: 'Nazwisko', validationF: defaultValidation},
+    pesel: {label: 'PESEL', validationF: peselValidation},
+    birthDate:{label: 'Data urodzenia', validationF: defaultValidation},
+    age: {label: 'Wiek', validationF: (value) => {
+        return value > 0 && /.+/.test(value)
+    }},
+    email: {label: 'Email', validationF: (value) => {
+        return regForEmail.test(value)
+    }},
+    details: {label: 'Opis', validationF: defaultValidation},
+    gender: {label: 'Płeć', validationF: defaultValidation},
+}
+const form = document.forms['userForm'];
+
 const handlePesel = (e) =>  {
     let pesel = e.target.value;
     if(pesel.length == 11){
@@ -36,10 +77,8 @@ const handlePesel = (e) =>  {
 
 const handleInputValidation = (e) => {
     let inputValue = e.target.value;
-    if(e.target.name=='pesel' && !peselValidation(inputValue)){
-        e.target.classList.add("wrongInput");
-    }
-    else if(!defaultValidation(inputValue)){
+    let validF = inputs[e.target.name].validationF
+    if(!validF(inputValue)){
         e.target.classList.add("wrongInput");
     }
     else{
@@ -48,36 +87,17 @@ const handleInputValidation = (e) => {
 }
 
 document.getElementById('pesel').addEventListener('input', handlePesel)
-const inputsForLiveValidation = document.querySelectorAll('input:not(input[type="button"])')
-console.log(inputsForLiveValidation);
 
-for (let i=0; i<inputsForLiveValidation.length; i++){
-    inputsForLiveValidation[i].addEventListener('focusout', handleInputValidation);
-}
+//@NOTE "obrzydliwe"
+// const inputsForLiveValidation = document.querySelectorAll('input:not(input[type="button"])')
 
+// for (let i=0; i<inputsForLiveValidation.length; i++){
+//     inputsForLiveValidation[i].addEventListener('focusout', handleInputValidation);
+// }
 
-
-const defaultValidation = (value) => {
-    return /.+/.test(value)
-}
-
-const peselValidation = (value) => {
-    let weights = '1379137913';
-    let checkSum = 0;
-    if(/^\d{11}$/.test(value)){
-        let i=0;
-        let tempSum;
-        for(i; i<10; i++){
-            tempSum = value[i] * weights[i];
-            tempSum = tempSum % 10;
-            checkSum += tempSum;
-        }
-        checkSum = checkSum % 10;
-        finalCheck = 10 - checkSum;
-        if(finalCheck == value[10]){
-            return true;
-        }
-    }
+for(inputName in inputs){
+    form[inputName].addEventListener('focusout', handleInputValidation);
+    form[inputName].addEventListener('input', handleInputValidation);
 }
 
 const isEven = (num) => num % 2 === 0;
@@ -85,22 +105,6 @@ const isEven = (num) => num % 2 === 0;
 const addZero = (num) => num = '0' + num;
 
 function getFormData(){
-    const regForEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    let inputs = {
-        name: {label: 'Imię', validationF: defaultValidation},
-        surname: {label: 'Nazwisko', validationF: defaultValidation},
-        pesel: {label: 'PESEL', validationF: peselValidation},
-        birthDate:{label: 'Data urodzenia', validationF: defaultValidation},
-        age: {label: 'Wiek', validationF: (value) => {
-            return value > 0 && /.+/.test(value)
-        }},
-        email: {label: 'Email', validationF: (value) => {
-            return regForEmail.test(value)
-        }},
-        details: {label: 'Opis', validationF: defaultValidation},
-        gender: {label: 'Płeć', validationF: defaultValidation},
-    }
-    const form = document.forms['userForm'];
     let alertMsg="Formularz źle wpełniony sprawdź pole: ";
     let output = '';
     let valid = true;
