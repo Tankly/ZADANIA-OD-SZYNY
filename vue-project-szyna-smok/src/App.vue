e75d325258424fc986d3384e7b46826d1bba7a25
Konrad Przybysz <k.przybysz@atcomp.pl>
2022-07-26 15:55:25 +0200

    poczatek VUE

---
+++ vue-project-szyna-smok/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Navbar :pages="pages"/>
+  <router-view/>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue"
+
+export default{
+  name: 'app',
+  data(){
+    return{
+      pages: {
+        home: {
+          
+        }
+      }
+    }
+  },
+  components:{
+    Navbar
+  }
+}
+</script>
---
+++ vue-project-szyna-smok/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar">
+        <div class="navbarBody">
+            <div v-for="page in pages" :key="page" class="navbarItem">
+                {{ page }}
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        props: ['pages'],
+    }
+</script>
